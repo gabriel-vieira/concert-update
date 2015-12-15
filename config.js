6d7ff0412effecd4784a3052fa0d7b76349588ae
gabriel-vieira <gabriel93250@gmail.com>
2015-12-16 00:19:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ config.deezer = {};
 
 config.deezer.cliendID = "";
 config.deezer.cliendSecret = "";
+config.deezer.host = 'api.deezer.com';
+config.deezer.port = 80;
+config.deezer.path = '/user/me/history?access_token=';
 
 module.exports = config;
