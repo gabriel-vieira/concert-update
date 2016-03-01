abb235c80a97d5cdaf2a9813878e2fcc8a20a388
Gabriel Vieira <gabriel.viera@simple-it.fr>
2016-03-01 11:21:46 +0100

    Revert "Revert "refacto""
    This reverts commit fe657337cb98ea50b9ac2fe4ed209fd4163f831a.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {};
 config.deezer = {};
 
-config.deezer.cliendID = "";
-config.deezer.cliendSecret = "";
+config.deezer.cliendID = "163445";
+config.deezer.cliendSecret = "911477ed1567cd35b8279a4bc3931567";
 config.deezer.host = 'api.deezer.com';
 config.deezer.port = 80;
 config.deezer.pathInfoUser = '/user/me?access_token=';
