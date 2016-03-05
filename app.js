425c769adc7a13b24106df0495e1449934f6ac2e
gabriel <gabriel@gabriel>
2016-03-06 00:01:28 +0100

    refacto deezer lig

--- app.js
+++ app.js
@@ -1,71 +1,11 @@
-function getInfosUser (accessToken, refreshToken, profile, done) {
-
-  var urlToGetHistoryUser = 'http://'+ config.deezer.host + config.deezer.pathHistoryUser + accessToken;
-
-  listSongs = _getHistory(urlToGetHistoryUser);
-
-  return done(null, profile);  
-}
-
-function _sortHistory (dataFromDeezer) {
-
-  var listSorted = [];
-  for (song in dataFromDeezer) {
-
-    var artistName = dataFromDeezer[song].artist.name;
-
-    if (listSorted.indexOf(artistName)  == -1) {
-        listSorted.push(artistName);
-    };
-  }
-
-  return listSorted;
-}
-
-function _getHistory (urlAPI) {
-  request(urlAPI, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-      var result = {
-        data: JSON.parse(body).data,
-        next: JSON.parse(body).next,
-        total: JSON.parse(body).total,
-      };
-      _getNextHistory(result); 
-    }
-  });
-}
-
-function _getNextHistory (history) {
-  request(history.next, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-        history.data = history.data.concat(JSON.parse(body).data);
-        history.next = JSON.parse(body).next;
-
-        if(_getHistoryIndexFromUrlAPI(history.next) < history.total) {
-          _getNextHistory(history);
-        } else {
-          var listArtistSorted = _sortHistory(history.data);
-          controllers.sendHistoryDataInFormatJSON(listArtistSorted);
-        }
-    }
-  });  
-}
-
-function _getHistoryIndexFromUrlAPI (urlAPI) {
-  var result = urlAPI.substr(urlAPI.lastIndexOf("&index=")+7);
-  return result;
-}
-
 var passport = require('passport')
   , util = require('util')
   , DeezerStrategy = require('passport-deezer').Strategy
   , http = require('http')
   , mongoose = require('mongoose')
   , config = require('./config')
-  , request = require('request')
   , promise = require('promise')
+  , deezer = require('./lib/deezer')
   , controllers = require('./controllers');
 
 // Passport session setup.
@@ -95,7 +35,7 @@ passport.use(new DeezerStrategy({
 
   },
   function(accessToken, refreshToken, profile, done) {
-    getInfosUser(accessToken, refreshToken, profile, done);
+    deezer.getInfosUser(accessToken, refreshToken, profile, done);
   }
 ));
 
