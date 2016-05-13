4e93a9328d2a3673c760147c1376d63c454cf029
gabriel <gabriel@gabriel>
2016-05-13 17:49:03 +0200

    cleaning with JSHINT

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ $(function(){
 		  $.each( data.history.data, function( key, val ) {
 		    items.push( "<li id='" + key + "'>" + val.artist.name + "</li>" );
 		  });
-		 
+
 		  $( "<ul/>", {
 		    "class": "my-new-list",
 		    html: items.join( "" )
