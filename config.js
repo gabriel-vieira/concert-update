0790b3be920bd920a4ac0dd23423ec40fc77af4c
gabriel-vieira <gabriel93250@gmail.com>
2016-05-13 17:04:24 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {};
+config.deezer = {};
+config.songKick = {};
+
+config.deezer.cliendID = "";
+config.deezer.cliendSecret = "";
+config.deezer.host = 'http://api.deezer.com';
+config.deezer.port = 80;
+config.deezer.pathInfoUser = '/user/me?access_token=';
+config.deezer.pathHistoryUser = '/user/me/history?access_token=';
+
+config.songKick.apiKey = ""
+
+module.exports = config;
