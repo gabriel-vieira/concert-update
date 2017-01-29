0a707068d8b5ecdf9ce1fcf3cb7bd260b2abb714
gabriel <gabriel@gabriel>
2017-01-29 18:28:03 +0100

    change organisation
    :q

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var passport = require('passport')
-  , util = require('util')
-  , DeezerStrategy = require('passport-deezer').Strategy
-  , http = require('http')
-  , mongoose = require('mongoose')
-  , config = require('./config')
-  , promise = require('promise')
-  , deezer = require('./lib/deezer')
-  , controllers = require('./controllers');
+var passport = require('passport'),
+  http = require('http'),
+  DeezerStrategy = require('passport-deezer').Strategy,
+  config = require('./config'),
+  express = require('express')
+  morgan = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  expressLayouts = require('express-ejs-layouts'),
+  methodOverride = require('method-override'),
+  session = require('express-session');
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -16,10 +18,12 @@ var passport = require('passport')
 //   have a database of user records, the complete Deezer profile is serialized
 //   and deserialized.
 passport.serializeUser(function(user, done) {
+  console.log('serializeUser user',user);
   done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
+  console.log('deserializeUser user',obj);
   done(null, obj);
 });
 
@@ -35,9 +39,77 @@ passport.use(new DeezerStrategy({
 
   },
   function(accessToken, refreshToken, profile, done) {
-    deezer.getInfosUser(accessToken);
+    console.log('profile', profile);
     return done(null, profile);
   }
 ));
 
-controllers.set(passport);
+var app = express();
+
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(expressLayouts)
+app.use(morgan('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({ secret: 'keyboard cat' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login')
+}
+
+app.get('/', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ user: req.user }));
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/deezer
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Deezer authentication will involve redirecting
+//   the user to deezer.com.  After authorization, Deezer will redirect the user
+//   back to this application at /auth/deezer/callback
+app.get('/auth/deezer',
+  passport.authenticate('deezer'),
+  function(req, res){
+    // The request will be redirected to Deezer for authentication, so this
+    // function will not be called.
+  }
+);
+
+// GET /auth/deezer/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/deezer/callback',
+  passport.authenticate('deezer', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ user: req.user }));
+  }
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+var server = app.listen(3000);
--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const config = {};
 config.deezer = {};
 config.songKick = {};
 
-config.deezer.cliendID = "";
-config.deezer.cliendSecret = "";
+config.deezer.cliendID = "163445";
+config.deezer.cliendSecret = "911477ed1567cd35b8279a4bc3931567";
 config.deezer.host = 'http://api.deezer.com';
 config.deezer.port = 80;
 config.deezer.pathInfoUser = '/user/me?access_token=';
 config.deezer.pathHistoryUser = '/user/me/history?access_token=';
 
-config.songKick.apiKey = ""
+config.songKick.apiKey = "6swWmYaRna026ddt"
 
 module.exports = config;
